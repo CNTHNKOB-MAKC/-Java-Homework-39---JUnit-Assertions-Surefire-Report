0b8da69e998821aefba5c50cd505741588bf9bc6
Maxim <atomicid@outlook.com>
2015-02-03 19:49:54 -0800

    Junit initial commit

---
+++ src/main/java/core/HW39_JUnitAssertionsSurefireReport/AppHW39.java
@@ -0,0 +1,17 @@
+package core.HW39_JUnitAssertionsSurefireReport;
+
+public class AppHW39 {
+	public static String s = "Testing JUnit 4";
+
+	 
+	public static Integer i = 55;
+
+	 
+	public static Boolean t = true;
+
+	 
+	public static Boolean f = false;
+	
+	public static void main( String[] args ){}
+	
+}
